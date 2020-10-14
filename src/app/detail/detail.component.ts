f6350e07265351b55ef2d49401d11ecc31b547c5
nguyenthuha5999 <70285724+nguyenthuha5999@users.noreply.github.com>
2020-10-14 22:26:32 +0700

    nguyenthuha5999

--- src/app/detail/detail.component.ts
+++ src/app/detail/detail.component.ts
@@ -1,29 +1,15 @@
-import { Component, OnInit, Injector } from '@angular/core';
-import { takeUntil } from 'rxjs/operators';
-import { BaseComponent } from './../lib/base-component';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-detail',
   templateUrl: './detail.component.html',
   styleUrls: ['./detail.component.css']
 })
-export class DetailComponent extends BaseComponent implements OnInit {
-  item:any;
-  constructor(injector: Injector) { 
-    super(injector);
-  }
+export class DetailComponent implements OnInit {
+
+  constructor() { }
+
   ngOnInit(): void {
-    this.item = {};
-    this._route.params.subscribe(params => {
-      let id = params['id'];
-      this._api.get('/api/item/get-by-id/'+id).pipe(takeUntil(this.unsubscribe)).subscribe(res => {
-        this.item = res;
-        setTimeout(() => {
-          this.loadScripts();
-        });
-      }); 
-    });
-
   }
-}
 
+}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,28 +1,15 @@
-import { Component, OnInit, Injector } from '@angular/core';
-import { BaseComponent } from './../lib/base-component';
-import { Observable} from 'rxjs';
-import 'rxjs/add/observable/combineLatest';
-import 'rxjs/add/operator/takeUntil';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent extends BaseComponent implements OnInit {
-  list_item:any;
-  constructor(injector: Injector) { 
-    super(injector);
-  }
+export class HomeComponent implements OnInit {
+
+  constructor() { }
 
   ngOnInit(): void {
-    Observable.combineLatest(
-      this._api.get('/api/item/get-all'),
-    ).takeUntil(this.unsubscribe).subscribe(res => {
-      this.list_item = res[0];
-      setTimeout(() => {
-        this.loadScripts();
-      });
-    }, err => { });
   }
+
 }
